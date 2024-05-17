87c7dd2bfe4ba0d4a458c8d2bbb1df04df007a1b
Nikster3k <144370425+Nikster3k@users.noreply.github.com>
2024-05-17 18:02:28 +0200

    FEATURE: reading from client socket and writing it into member head and body string working (maybe). Need request and response implementations. Operator overload with Connect and const int, to search for connection in vector with fd

--- Connection.hpp
+++ Connection.hpp
@@ -1,13 +1,38 @@
-#ifndef CONNECTION_H
-# define CONNECTION_H
+#ifndef CONNECTION_HPP
+# define CONNECTION_HPP
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFFER_SIZE 16
 
 class Connection
 {
-public:
+private:
+
+	int			m_clientSocket;
+
+	std::string	m_head;
+	std::string	m_body;
+
+	std::string	readUntilSep(std::string& a_ouput, const std::string& a_seperator);
 	Connection(void);
+
+public:
+
+	Connection(int a_clientSocket);
 	Connection(const Connection& a_other);
 	Connection& operator=(const Connection& a_other);
 	~Connection();
+
+	int	getSocketFd(void) const;
+	int	reciveRequestRaw(void);
+
+	bool operator==(const int a_fd) const;
+
+	//debug
+	void printHeadNBody(void) const;
 };
 
-#endif // !CONNECTION_H
+#endif // !CONNECTION_HPP
