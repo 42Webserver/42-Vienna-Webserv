cc7392a287f65574e4a73f27ee465c0c9e620ecb
windchaser-surf <wechslerf@gmail.com>
2024-05-21 12:18:17 +0200

    FEATURE: Added global var for status codes

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include "Dummy_Request.hpp"
 #include "Response.hpp"
 
+std::map<std::string, std::string> g_status_codes;
+
+
+void initStatusCodes()
+{
+    g_status_codes["200"] = "OK";
+    g_status_codes["400"] = "Bad Request";
+    g_status_codes["404"] = "Not Found";
+    g_status_codes["405"] = "Method Not Allowed";
+    g_status_codes["414"] = "URI Too Long";
+    g_status_codes["500"] = "Internal Server Error";
+    g_status_codes["505"] = "HTTP Version not supported";
+}
+
 int main() 
 {
     std::map<std::string, std::string> test; 
 
+    initStatusCodes();
+
     test["method"] = "GET";
-    test["uri"] = "index.html";
+    test["uri"] = "index.htdml";
     test["http_version"] = "HTTP/1.1";
 
     Dummy_Request testRequest(test);
