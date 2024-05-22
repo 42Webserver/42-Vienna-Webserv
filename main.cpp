bdbc32c3581c907f389ade57fc4f77ac6eb1e584
floheid98 <florian.heid98@googlemail.com>
2024-05-22 15:21:46 +0200

    ADD: added main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "Webserver.hpp"
+
+std::map<std::string, std::string> g_status_codes;
+
+
+void initStatusCodes()
+{
+    g_status_codes["200"] = "OK";
+    g_status_codes["400"] = "Bad Request";
+    g_status_codes["404"] = "Not Found";
+    g_status_codes["405"] = "Method Not Allowed";
+    g_status_codes["414"] = "URI Too Long";
+    g_status_codes["500"] = "Internal Server Error";
+    g_status_codes["505"] = "HTTP Version not supported";
+}
+
+int	main()
+{
+	Webserver webserv;
+
+	initStatusCodes();
+	webserv.run();
+}
