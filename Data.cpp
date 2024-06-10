24fdc4d55a3896760ac9bd636237075525faf16b
windchaser-surf <wechslerf@gmail.com>
2024-06-10 17:50:52 +0200

    FIX: implemented subserver struct with map subserver and vector of locations

--- Data.cpp
+++ Data.cpp
@@ -1,11 +1,11 @@
 #include "Data.hpp"
 
-void printData(std::vector <std::map< std::string, std::vector<std::string> > > data)
+void printData(std::vector <struct subserver> data)
 {
 	for (size_t i = 0; i < data.size(); i++)
 	{
 		std::cout << "Server number " << i << std::endl;
-		for (std::map<std::string, std::vector<std::string> > ::iterator it = data.at(i).begin(); it != data.at(i).end(); ++it)
+		for (std::map<std::string, std::vector<std::string> > ::iterator it = data.at(i).server.begin(); it != data.at(i).server.end(); ++it)
 		{
 			std::cout << "	Key: " << it->first << " | value: ";
 			for (size_t i = 0; i < it->second.size(); i++)
@@ -14,6 +14,24 @@ void printData(std::vector <std::map< std::string, std::vector<std::string> > > 
 			}
 			std::cout<< std::endl;
 		}
+		std::cout << "Location: \n";
+		
+	for (size_t j = 0; j < data.at(i).location.size(); j++)
+	{
+		std::cout << "Location number: " << j << std::endl;
+		for (std::map<std::string, std::vector<std::string> > ::iterator it = data.at(i).location.at(j).begin(); it != data.at(i).location.at(j).end(); ++it)
+		{
+			std::cout << "	Key: " << it->first << " | value: ";
+			for (size_t j = 0; j < it->second.size(); j++)
+			{
+				std::cout << it->second.at(j) << ", ";
+			}
+			std::cout<< std::endl;
+		}
+		std::cout << "Location: \n";
+		
+		
+	}
 		
 	}
 	
@@ -21,13 +39,13 @@ void printData(std::vector <std::map< std::string, std::vector<std::string> > > 
 
 bool safeData(std::vector<std::string> tokens)
 {
-	std::vector<std::map<std::string, std::vector<std::string> > > data;
+	std::vector<struct subserver> data;
 	for (size_t i = 0; i < tokens.size(); i++)
 	{
 		if (tokens.at(i) == "Server")
 		{
 			i += 2;
-			std::map<std::string, std::vector<std::string> > newSubserver;
+			struct subserver newSubserver;
 			initSubserver(newSubserver);
 			//Already skip the opening brackets of serverscope
 			int countScopes = 1;
@@ -44,7 +62,7 @@ bool safeData(std::vector<std::string> tokens)
 					continue;
 				}
 				//key = string
-				if (newSubserver.find(tokens.at(i)) != newSubserver.end())
+				if (newSubserver.server.find(tokens.at(i)) != newSubserver.server.end())
 				{
 					//Add exactly one
 					if (tokens.at(i + 1) == ";")
@@ -55,12 +73,12 @@ bool safeData(std::vector<std::string> tokens)
 
 					//ADD just one arg!
 					if (tokens.at(i) == "listen" || tokens.at(i) == "root" || tokens.at(i) == "index" \
-						|| tokens.at(i) == "client_max_body_size")
+						|| tokens.at(i) == "client_max_body_size" || tokens.at(i) == "autoindex")
 					{
 						i++;
 						std::vector <std::string> value;
 						value.push_back(tokens.at(i));
-						newSubserver[tokens.at(i - 1)] = value;
+						newSubserver.server[tokens.at(i - 1)] = value;
 						i++;
 						if (tokens.at(i) != ";")
 						{
@@ -76,15 +94,58 @@ bool safeData(std::vector<std::string> tokens)
 						std::vector<std::string> value;
 						while (tokens.at(i) != ";")
 							value.push_back(tokens.at(i++));
-						newSubserver[tokens.at(i - value.size() - 1)] = value;
+						newSubserver.server[tokens.at(i - value.size() - 1)] = value;
 					}
 					//Add location
 					}
 					else if (tokens.at(i) == "location")
 					{
+						std::map< std::string, std::vector<std::string> > location;
+						initLocation(location, newSubserver);
 						std::cout << "Found a location! and skip it now!" << std::endl;
+						i += 3;
+
+
+
+
 						while(tokens.at(i) != "}")
-						i++;
+						{
+							if (tokens.at(i) == "listen" || tokens.at(i) == "root" || tokens.at(i) == "index" \
+								|| tokens.at(i) == "client_max_body_size" || tokens.at(i) == "autoindex")
+							{
+								i++;
+								std::vector <std::string> value;
+								value.push_back(tokens.at(i));
+								location[tokens.at(i - 1)] = value;
+								i++;
+								if (tokens.at(i) != ";")
+								{
+									std::cerr << "Two many arguments in location for key: " << tokens.at(i - 2);
+									return (false);
+								}
+							}
+							else if (tokens.at(i) == "server_name" || tokens.at(i) == "error_page" || \
+								tokens.at(i) == "allowed_methods" || tokens.at(i) == "return")
+							{
+								i++;
+								std::vector<std::string> value;
+								while (tokens.at(i) != ";")
+									value.push_back(tokens.at(i++));
+								location[tokens.at(i - value.size() - 1)] = value;
+							}
+							else 
+							{
+								std::cout << "Trash not allowed in location: " << tokens.at(i) <<  std::endl;
+								std::cout << "Befor trash" << tokens.at(i - 1) << std::endl;
+								return (false);
+							}
+							i++;
+						}
+
+
+
+						newSubserver.location.push_back(location);
+						// newSubserver[""]
 					}
 					else 
 					{
@@ -108,18 +169,28 @@ bool safeData(std::vector<std::string> tokens)
 	return false;
 }
 
-void initSubserver(std::map <std::string, std::vector<std::string> > &subserver)
+void initSubserver(struct subserver &subserver)
 {
-	subserver["listen"];
-	subserver["root"];
-	subserver["index"];
-	subserver["server_name"];
-	subserver["client_max_body_size"];
-	subserver["error_page"];
-	subserver["allowed_methods"];
-	subserver["autoindex"];
-	subserver["return"];
-	subserver["locations"];
-
+	subserver.server["listen"];
+	subserver.server["root"];
+	subserver.server["index"];
+	subserver.server["server_name"];
+	subserver.server["client_max_body_size"];
+	subserver.server["error_page"];
+	subserver.server["allowed_methods"];
+	subserver.server["autoindex"];
+	subserver.server["return"];
+	subserver.server["locations"];
 }
 
+void initLocation(std::map<std::string, std::vector<std::string> > &location, struct subserver newSubserver)
+{
+	location["index"] = newSubserver.server["index"];
+	location["client_max_body_size"] = newSubserver.server["client_max_body_size"];
+	location["error_page"] = newSubserver.server["error_page"];
+	location["allowed_methods"] = newSubserver.server["allowed_methods"];
+	location["autoindex"] = newSubserver.server["autoindex"];
+	location["return"] = newSubserver.server["return"];
+	location["root"] = newSubserver.server["root"];
+
+}
--- Data.hpp
+++ Data.hpp
@@ -19,20 +19,11 @@ struct location
 
 struct subserver
 {
-    std::string listen; //1
-    std::string root;   // 1
-    std::string index; // 1
-    std::vector<std::string> server_name; //min 1
-    int client_max_body_size;   //1
-    std::vector<std::string> error_page; //min 1
-    std::vector<std::string> allowed_methods; //min 1
-    bool autoindex; //on || off
-    std::vector<std::string> redir; //min 1 
-    std::vector<location> locations; //optional
+    std::map< std::string, std::vector<std::string> >server;
+    std::vector <std::map< std::string, std::vector<std::string> > > location;
 };
-
-
 bool    safeData(std::vector<std::string> tokens);
-void    initSubserver(std::map <std::string, std::vector<std::string> > &subserver);
+void    initSubserver(struct subserver &subserver);
+void    initLocation(std::map<std::string, std::vector<std::string> > &location, struct subserver newSubserver);
 
 #endif // !DATA_H
