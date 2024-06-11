6d4ac1412e74bbdfa1a29840419b2a0ba8fe19f3
windchaser-surf <wechslerf@gmail.com>
2024-06-11 15:40:54 +0200

    FIX: add location_name to location map, shorted functions, throw exceptions in case of errors

--- Data.cpp
+++ Data.cpp
@@ -16,25 +16,55 @@ void printData(std::vector <struct subserver> data)
 		}
 		std::cout << "Location: \n";
 		
-	for (size_t j = 0; j < data.at(i).location.size(); j++)
-	{
-		std::cout << "Location number: " << j << std::endl;
-		for (std::map<std::string, std::vector<std::string> > ::iterator it = data.at(i).location.at(j).begin(); it != data.at(i).location.at(j).end(); ++it)
+		for (size_t j = 0; j < data.at(i).location.size(); j++)
 		{
-			std::cout << "	Key: " << it->first << " | value: ";
-			for (size_t j = 0; j < it->second.size(); j++)
+			std::cout << "Location number: " << j << std::endl;
+			for (std::map<std::string, std::vector<std::string> > ::iterator it = data.at(i).location.at(j).begin(); it != data.at(i).location.at(j).end(); ++it)
 			{
-				std::cout << it->second.at(j) << ", ";
+				std::cout << "		Key: " << it->first << " | value: ";
+				for (size_t j = 0; j < it->second.size(); j++)
+				{
+					std::cout << it->second.at(j) << ", ";
+				}
+				std::cout<< std::endl;
 			}
-			std::cout<< std::endl;
+		}	
+	}
+}
+
+bool getLocation(struct subserver &newSubserver, std::vector<std::string> &tokens, size_t &i)
+{
+	std::map< std::string, std::vector<std::string> > location;
+	initLocation(location, newSubserver);
+	std::vector<std::string> value;
+	value.push_back(tokens.at(i + 1));
+	location["name"] = value;
+	i += 3;
+	while(tokens.at(i) != "}")
+	{
+		value.clear();
+		if (tokens.at(i) == "listen" || tokens.at(i) == "root" || tokens.at(i) == "index" \
+			|| tokens.at(i) == "client_max_body_size" || tokens.at(i) == "autoindex")
+		{
+			value.push_back(tokens.at(++i));
+			location[tokens.at(i - 1)] = value;
+			if (tokens.at(++i) != ";")
+				throw std::runtime_error("Error: config-file: Two many arguments for key [locationscope]");
 		}
-		std::cout << "Location: \n";
-		
-		
+		else if (tokens.at(i) == "server_name" || tokens.at(i) == "error_page" || \
+			tokens.at(i) == "allowed_methods" || tokens.at(i) == "return")
+		{
+			i++;
+			while (tokens.at(i) != ";")
+				value.push_back(tokens.at(i++));
+			location[tokens.at(i - value.size() - 1)] = value;
+		}
+		else
+			throw std::runtime_error("Error: config-file: Trash not allowed [locationscope]"); 
+		i++;
 	}
-		
-	}
-	
+	newSubserver.location.push_back(location);
+	return (true);
 }
 
 bool safeData(std::vector<std::string> tokens)
@@ -55,10 +85,7 @@ bool safeData(std::vector<std::string> tokens)
 				{
 					tokens.at(i) == "{" ? countScopes++ : countScopes--;
 					if (countScopes == 0)
-					{
-						std::cout << "Finished server scope! | i = "<< i <<  std::endl;
 						break;
-					}
 					continue;
 				}
 				//key = string
@@ -66,11 +93,7 @@ bool safeData(std::vector<std::string> tokens)
 				{
 					//Add exactly one
 					if (tokens.at(i + 1) == ";")
-					{
-						std::cout << "Missing argument at key = " << tokens.at(i) << std::endl;
-						return (false);
-					}
-
+						throw std::runtime_error("Error: config-file: Missing argument at key [serverscope]");
 					//ADD just one arg!
 					if (tokens.at(i) == "listen" || tokens.at(i) == "root" || tokens.at(i) == "index" \
 						|| tokens.at(i) == "client_max_body_size" || tokens.at(i) == "autoindex")
@@ -81,10 +104,7 @@ bool safeData(std::vector<std::string> tokens)
 						newSubserver.server[tokens.at(i - 1)] = value;
 						i++;
 						if (tokens.at(i) != ";")
-						{
-							std::cerr << "Two many arguments for key: " << tokens.at(i - 2);
-							return (false);
-						}
+							throw std::runtime_error("Error: config-file: Two many arguments for key [serverscope]");
 					}
 					//ADD optional multiple arguments!
 					else if (tokens.at(i) == "server_name" || tokens.at(i) == "error_page" || \
@@ -97,76 +117,20 @@ bool safeData(std::vector<std::string> tokens)
 						newSubserver.server[tokens.at(i - value.size() - 1)] = value;
 					}
 					//Add location
-					}
-					else if (tokens.at(i) == "location")
-					{
-						std::map< std::string, std::vector<std::string> > location;
-						initLocation(location, newSubserver);
-						std::cout << "Found a location! and skip it now!" << std::endl;
-						i += 3;
-
-
-
-
-						while(tokens.at(i) != "}")
-						{
-							if (tokens.at(i) == "listen" || tokens.at(i) == "root" || tokens.at(i) == "index" \
-								|| tokens.at(i) == "client_max_body_size" || tokens.at(i) == "autoindex")
-							{
-								i++;
-								std::vector <std::string> value;
-								value.push_back(tokens.at(i));
-								location[tokens.at(i - 1)] = value;
-								i++;
-								if (tokens.at(i) != ";")
-								{
-									std::cerr << "Two many arguments in location for key: " << tokens.at(i - 2);
-									return (false);
-								}
-							}
-							else if (tokens.at(i) == "server_name" || tokens.at(i) == "error_page" || \
-								tokens.at(i) == "allowed_methods" || tokens.at(i) == "return")
-							{
-								i++;
-								std::vector<std::string> value;
-								while (tokens.at(i) != ";")
-									value.push_back(tokens.at(i++));
-								location[tokens.at(i - value.size() - 1)] = value;
-							}
-							else 
-							{
-								std::cout << "Trash not allowed in location: " << tokens.at(i) <<  std::endl;
-								std::cout << "Befor trash" << tokens.at(i - 1) << std::endl;
-								return (false);
-							}
-							i++;
-						}
-
-
-
-						newSubserver.location.push_back(location);
-						// newSubserver[""]
-					}
-					else 
-					{
-						std::cout << "Trash not allowed: " << tokens.at(i) <<  std::endl;
-						std::cout << "Befor trash" << tokens.at(i - 1) << std::endl;
-						return (false);
-					}
+				}
+				else if (tokens.at(i) == "location")
+					getLocation(newSubserver, tokens, i);
+				else
+					throw std::runtime_error("Error: config-file: Trash not allowed [serverscope]");
 			}
 			data.push_back(newSubserver);
 		}
-		else 
-		{
-			std::cout << "Trash between scopes!: " << tokens.at(i) << "pos: " << i << std::endl;
-			return (false);
-		}
-
+		else
+			throw std::runtime_error("Error: config-file: Trash between scopes!");
 	}
-
 	printData(data);
 	std::cout << "Number of subservers: " << data.size() << std::endl;
-	return false;
+	return (true);
 }
 
 void initSubserver(struct subserver &subserver)
@@ -192,5 +156,5 @@ void initLocation(std::map<std::string, std::vector<std::string> > &location, st
 	location["autoindex"] = newSubserver.server["autoindex"];
 	location["return"] = newSubserver.server["return"];
 	location["root"] = newSubserver.server["root"];
-
+	location["name"];
 }
--- Data.hpp
+++ Data.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <iostream>
 #include <map>
+#include <exception>
 
 struct subserver
 {
