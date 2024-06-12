21d249ddc1d7ed3ebd596d70ff47fe4e54411ea9
floheid98 <florian.heid98@googlemail.com>
2024-06-12 15:26:41 +0200

    FIX: added struct subserver to Data.hpp instead of all other functions

--- Data.hpp
+++ Data.hpp
@@ -3,20 +3,12 @@
 
 #include <string>
 #include <vector>
-#include <iostream>
 #include <map>
-#include <exception>
 
 struct subserver
 {
     std::map< std::string, std::vector<std::string> > server;
     std::vector <std::map< std::string, std::vector<std::string> > > locations;
 };
-
-std::vector<struct subserver>    safeData(std::vector<std::string> tokens);
-void    initSubserver(struct subserver &subserver);
-void    initLocation(std::map<std::string, std::vector<std::string> > &location);
-void    updateLocation(std::map<std::string, std::vector<std::string> > &location, struct subserver newSubserver);
-void    printData(std::vector <struct subserver> data);
 
 #endif // !DATA_H
