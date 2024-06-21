43f5cdf7e9a72ef90a8679883b0924b8434e5932
Nikster3k <144370425+Nikster3k@users.noreply.github.com>
2024-06-21 18:40:10 +0200

    FIX: big change in connection and request. Moved head and body out of connection. Request now got more get methods and methods to set head and body of a request.

--- Request.hpp
+++ Request.hpp
@@ -16,7 +16,7 @@ public:
 
 	Request(void);
 	Request(const Request& other);
-	Request(std::string& head);
+	// Request(std::string& head);
 	Request& operator=(const Request& other);
 	~Request();
 
@@ -27,15 +27,22 @@ public:
 	std::string			getRequestHost() const;
 	bool				getIsValid(void) const;
 	size_t				getContentLength() const;
+	void				addHead(const std::string& a_head);
+	bool				headComplete(void);
 	void				addBody(const std::string& a_body);
-	bool				requestComplete(void) const;
-	const std::string&	getBody();	
+	bool				bodyComplete(void) const;
+	bool				isReady(void);
+	const std::string&	getHead();
+	const std::string&	getBody();
+	void				initMap();
+
 private:
 
 	void	getRequestLine(std::string& line);
 	void	createKeyValuePair(std::string line);
-	void	initMap(std::string head);
 	bool								m_isValid;
+	bool								m_headComplete;
+	std::string							m_head;
 	std::string							m_body;
 	std::map <std::string, std::string>	m_requestHeader;
 };
