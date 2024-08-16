0ac330c445aab875c5459fc5938290080f298f44
Nikster3k <144370425+Nikster3k@users.noreply.github.com>
2024-08-16 16:20:41 +0200

    FEATURE: response header is now map this allows the cgi to respond with values for the header and override them in the response header. This needs an extra method to convert the map into a correctly http response header.

--- src/Response.cpp
+++ src/Response.cpp
@@ -93,7 +93,7 @@ void Response::initContentType()
 
 const std::string Response::getResponse() const
 {
-	return (m_responseHeader + m_responseBody);
+	return (headerMapToString() + m_responseBody);
 }
 
 void Response::setValidMsg(const std::string &filepath)
@@ -462,98 +462,81 @@ void Response::createAutoIndex(std::string &a_path)
 
 //////////////////////+++++Response Header+++++++++++//////////////////////
 
+std::string Response::headerMapToString(void) const
+{
+	std::string headerStr;
+	std::map<std::string, std::string>::const_iterator statusLine = m_responseHeader.find("HTTP/1.1");
+	if (statusLine != m_responseHeader.end())
+		headerStr += "HTTP/1.1 " + statusLine->second + "\r\n";
+	for (std::map<std::string, std::string>::const_iterator i = m_responseHeader.begin(); i != m_responseHeader.end(); i++)
+	{
+		if (i == statusLine)
+			continue;
+		headerStr += i->first + ": " + i->second + "\r\n";
+	}
+	headerStr.append("\r\n");
+	return (headerStr);	
+}
+
 void Response::getResponseHeader(const std::string &a_status_code, const std::string &a_redirLoc, const std::string &a_content_type)
 {
-	std::string response_header;
 	m_responseHeader.clear();
-	addStatusLine(a_status_code, response_header);
-	addServerName(response_header);
-	addDateAndTime(response_header);
+	m_responseHeader["HTTP/1.1"] = a_status_code + ' ' + s_status_codes[a_status_code];
+	m_responseHeader["Server"] = SERVERNAME;
+	addDateAndTime();
 	//Content-type!
-	addContentType(response_header, a_content_type);
-	addContentLength(response_header);
+	addContentType(a_content_type);
+	addContentLength();
 	//Connection: keep-alive!
-	addConnection(a_status_code, response_header);
-	addRedirection(response_header, a_redirLoc);
-	response_header.append("\r\n");
-	m_responseHeader += response_header;
+	addConnection(a_status_code);
+	addRedirection(a_redirLoc);
 }
 
-void	Response::addStatusLine(const std::string &a_status_code, std::string& a_response_header)
-{
-	a_response_header.append("HTTP/1.1");
-	a_response_header += ' ';
-	a_response_header.append(a_status_code);
-	a_response_header += ' ';
-	a_response_header.append(s_status_codes[a_status_code]);
-	a_response_header.append("\r\n");
-}
-
-void Response::addDateAndTime(std::string &a_response_header)
+void Response::addDateAndTime()
 {
 	std::time_t t = std::time(NULL);
 	std::tm* now = std::localtime(&t);
 
 	char buffer[32];
-	a_response_header.append("Date: ");
 	strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S CEST", now);
 
-	a_response_header.append(buffer);
-	a_response_header.append("\r\n");
+	m_responseHeader["Date"] = buffer;
 }
 
-void Response::addConnection(const std::string& a_status_code, std::string &a_response_header)
+void Response::addConnection(const std::string& a_status_code)
 {
 	const std::string& requestConnection = m_request.getValue("Connection");
 	if (a_status_code > "302")
-		a_response_header.append("Connection: close\r\n");
+		m_responseHeader["Connection"] = "close";
 	else if (!requestConnection.empty())
-	{
-		a_response_header.append("Connection: ");
-		a_response_header.append(requestConnection);
-		a_response_header.append("\r\n");
-	}
+		m_responseHeader["Connection"] = requestConnection;
 }
 
-void Response::addContentLength(std::string &a_response_header)
+void Response::addContentLength()
 {
 	std::ostringstream convert;
 
 	convert << m_responseBody.length();
-	a_response_header.append("Content-Length: ");
-	a_response_header.append(convert.str());
-	a_response_header.append("\r\n");
+	m_responseHeader["Content-Length"] = convert.str();
 }
 
-void Response::addRedirection(std::string &a_response_header, const std::string &a_redLoc)
+void Response::addRedirection(const std::string &a_redLoc)
 {
 	if (m_eventFlags & REDIRECTION)
 	{
-		a_response_header.append("Location: ");
 		if (m_eventFlags & REDIR_LOCATION)
-			a_response_header.append(m_config.at("return").at(1));
+			m_responseHeader["Location"] = m_config.at("return").at(1);
 		else
-			a_response_header.append(a_redLoc);
-		a_response_header.append("\r\n");
-
+			m_responseHeader["Location"] = a_redLoc;
 	}
 }
 
-void Response::addServerName(std::string &a_response_header)
+void Response::addContentType(const std::string &a_content_type)
 {
-	a_response_header.append("Server: ");
-	a_response_header.append(SERVERNAME);
-	a_response_header.append("\r\n");
-}
-
-void Response::addContentType(std::string &a_response_header, const std::string &a_content_type)
-{
-	a_response_header.append("Content-Type: ");
 	if (s_content_type.find(a_content_type) != s_content_type.end())
-		a_response_header.append(s_content_type[a_content_type]);
+		m_responseHeader["Content-Type"] = s_content_type[a_content_type];
 	else
-		a_response_header.append("text/plain");
-	a_response_header.append("\r\n");
+		m_responseHeader["Content-Type"] = "text/plain";
 }
 
 
