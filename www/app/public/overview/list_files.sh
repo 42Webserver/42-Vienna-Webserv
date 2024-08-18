e9734d7a09ad7868a576d9604a33b6116fff9988
Felix <wechslerf@gmail.com>
2024-08-18 17:02:05 +0200

    ADD: new html page for listening a directory, based on bash script

---
+++ www/app/public/overview/list_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set the content type to JSON
+echo -e "Content-type: application/json\r\n\r\n"
+
+# Directory to list
+directory="/home/felix/Dokumente/webserver/src"
+
+# List the directory contents and format as JSON
+json_output="{\"files\": ["
+first=true
+for file in "$directory"/*; do
+    if [ "$first" = true ]; then
+        first=false
+    else
+        json_output+=", "
+    fi
+    json_output+="\"$(basename "$file")\""
+done
+json_output+="]}"
+
+# Output the JSON object
+echo "$json_output"
